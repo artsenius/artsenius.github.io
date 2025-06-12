709188b57c00040a82f7844239bed995b9ee5710
Arthur Senko <arthursenko@gmail.com>
2025-06-12 01:00:25 -0700

    done for today

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Layout from './components/Layout';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/about">
       <Layout>
         <Header />
         <Routes>
